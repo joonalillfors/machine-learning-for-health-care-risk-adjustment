c58398c816af5bed38a89f44ad07e6ebf6f30186
Joona Lillfors <joona.lillfors@aalto.fi>
2020-04-24 16:01:40 +0300

    Removed useless and fix typos

--- nhs_main.py
+++ nhs_main.py
@@ -23,18 +23,12 @@ def printResults(model, test_x, test_y, val_x, val_y, train_x, train_y, name):
 
 def main():
     # from practice info drop rurality, atypical characteristics and practice type (not in 15-16) and quarter used (not in 18-19)
-    # indcludecols15 = ['NHSEnglandRegionCode','CCGCode','PracticeCloseDate','ContractType','DispensingPractice','QuarterUsedForPatientData','NumberOfRegisteredPatientsLastKnownFigure','NumberOfWeightedPatientsLastKnownFigure','AveragePaymentPerRegisteredPatient_£','AveragePaymentPerWeightedPatient_£','TotalNHSPaymentsToGeneralPractice_£','DeductionsForPensionsLeviesAndPrescriptionChargeIncome_£','TotalNHSPaymentsToGeneralPracticeMinusDeductions_£']
-    indcludecols15 = ['NHSEnglandRegionCode','DispensingPractice','ContractType','NumberOfRegisteredPatientsLastKnownFigure','NumberOfWeightedPatientsLastKnownFigure','TotalNHSPaymentsToGeneralPractice_£']
-    indcludecols16 = ['NHSEnglandRegionLocalOfficeCode','ContractType','DispensingPractice','QuarterUsedForPatientData','NumberofRegisteredPatientsLastKnownFigure','NumberofWeightedPatientsLastKnownFigure','AveragePaymentsPerRegisteredPatient_£','AveragePaymentsPerWeightedPatient_£','TotalNHSPaymentsToGeneralPractice_£','DeductionsForPensionsLeviesAndPrescriptionChargeIncome_£','TotalNHSPaymentsToGeneralPracticeMinusDeductions_£']
-    indcludecols17 = ['NHS England (Region, local office) Code','CCG Code','Practice Code','Practice Postcode','Practice Open Date','Practice Close Date','Contract Type','Dispensing Practice','Quarter used for patient data','Number of Registered Patients (Last Known Figure)','Number of Weighted Patients (Last Known Figure)','Average payments per registered patient','Average payments per weighted patient','Total NHS Payments to General Practice',"Deductions for Pensions, Levies and Prescription Charge Income",'Total NHS Payments to General Practice Minus Deductions']
-    indcludecols18 = ['NHS England and NHS Improvement (Region, local office) Code','CCG Code','Practice Code','Practice Postcode','Practice Open Date','Practice Close Date','Contract Type','Dispensing Practice','Average Number of Registered Patients','Average Number of Weighted Patients','Average payments per registered patient','Average payments per weighted patient','Total NHS Payments to General Practice',"Deductions for Pensions, Levies and Prescription Charge Income",'Total NHS Payments to General Practice Minus Deductions']
-
-    excludecols15 = ['GlobalSum_£','MPIGCorrectionFactor_£','PremisesPayments_£','Seniority_£','DoctorsRetainerSchemePayments_£','TotalLocumAllowances_£','ProlongedStudyLeave_£','Appraisal_AppraiserCostsInRespectOfLocums_£','PCOAdminOther_£','TotalQOFPayments_£','Alcohol_£','ChildhoodVaccinationAndImmunisationScheme_£','ExtendedHoursAccess_£','FacilitatingTimelyDiagnosisAndSupportForPeopleWithDementia_£','ImprovingPatientOnlineAccess_£','InfluenzaAndPneumococcalImmunisations_£','LearningDisabilities_£','MinorSurgery_£','PatientParticipation_£','RemoteCareMonitoring_£','RiskProfilingAndCaseManagement_£','RotavirusAndShinglesImmunisation_£','ServicesForViolentPatients_£','UnplannedAdmissions_£','OutOfAreaInHoursUrgentCare_£','Meningitis_£','NationalEnhancedServices_£','LocalEnhancedServices_NHAIS_£','LocalEnhancedServices_ISFE_£','InformationManagementAndTechnology_£','BalanceOfPMSExpenditure_£','NonDESItemPneumococcalVaccineChildhoodImmunisationMainProgramme_£','PrescribingFeePayments_£','DispensingFeePayments_£','ReimbursementOfDrugs_£','OtherPayments_£']
-    excludecols16 = ['GlobalSum_£','MPIGCorrectionfactor_£','PremisesPayments_£','Seniority_£','DoctorsRetainerSchemePayments_£','TotalLocumAllowances_£','ProlongedStudyLeave_£','AppraisalAppraiserCostsinRespectofLocums_£','PCOAdminOther_£','TotalQOFPayments_£','ChildhoodVaccinationAndImmunisationScheme_£','ExtendedHoursAccess_£','FacilitatingTimelyDiagnosisAndSupportForPeopleWithDementia_£','InfluenzaandPneumococcalImmunisations_£','LearningDisabilities_£','MinorSurgery_£','RotavirusAndShinglesImmunisation_£','ServicesForViolentPatients_£','UnplannedAdmissions_£','OutOfAreaInHoursUrgentCare_£','Meningitis_£','TotalNationalEnhancedServices_£','TotalLocalEnhancedServices_£','GeneralPracticeForwardView_£','InformationManagementandTechnology_£','BalanceofPMSExpenditure_£','NonDESItemPneumococcalVaccineChildhoodImmunisationMainProgramme_£','PrescribingFeePayments_£','DispensingFeePayments_£','ReimbursementOfDrugs_£','OtherPayments_£']
-    excludecols17 = ['Global Sum','MPIG Correction factor','Premises Payments','Seniority','Doctors Retainer Scheme Payments','Total Locum Allowances','Prolonged Study Leave','Appraisal Costs ','PCO Admin Other','Total QOF Payments','Childhood Vaccination and Immunisation Scheme','Extended Hours Access','Facilitating Timely Diagnosis and Support for People with Dementia','Influenza and Pneumococcal Immunisations','Learning Disabilities','Minor Surgery','Rotavirus and Shingles Immunisation','Services for Violent Patients','Unplanned Admissions','Out Of Area in Hours Urgent Care','Meningitis','Pertussis','Total Local Enhanced Services','General Practice Forward View','Information Management and Technology','Balance of PMS Expenditure','Non DES Item Pneumococcal Vaccine, Childhood Immunisation Main Programme','Prescribing Fee Payments','Dispensing Fee Payments','Reimbursement of Drugs','Other Payments1']
-    excludecols18 = ['Global Sum','MPIG Correction factor','Balance of PMS Expenditure','Total QOF Payments','Childhood Vaccination and Immunisation Scheme','Extended Hours Access','Influenza and Pneumococcal Immunisations','Learning Disabilities','Meningitis','Minor Surgery','Out Of Area in Hours Urgent Care','Pertussis','Rotavirus and Shingles Immunisation','Services for Violent Patients','Total Local Incentive Schemes','Premises Payments','Seniority','Doctors Retainer Scheme Payments','Total Locum Allowances','Appraisal Costs ','Prolonged Study Leave','PCO Admin Other','Information Management and Technology','Non DES Item Pneumococcal Vaccine, Childhood Immunisation Main Programme','General Practice Forward View','Prescribing Fee Payments','Dispensing Fee Payments','Reimbursement of Drugs','Other Payments1']
+    # includecols15 = ['NHSEnglandRegionCode','CCGCode','PracticeCloseDate','ContractType','DispensingPractice','QuarterUsedForPatientData','NumberOfRegisteredPatientsLastKnownFigure','NumberOfWeightedPatientsLastKnownFigure','AveragePaymentPerRegisteredPatient_£','AveragePaymentPerWeightedPatient_£','TotalNHSPaymentsToGeneralPractice_£','DeductionsForPensionsLeviesAndPrescriptionChargeIncome_£','TotalNHSPaymentsToGeneralPracticeMinusDeductions_£']
+    includecols15 = ['NHSEnglandRegionCode','DispensingPractice','ContractType','NumberOfRegisteredPatientsLastKnownFigure','NumberOfWeightedPatientsLastKnownFigure','TotalNHSPaymentsToGeneralPractice_£']
+    includecols17 = ['NHS England (Region, local office) Code','CCG Code','Practice Code','Practice Postcode','Practice Open Date','Practice Close Date','Contract Type','Dispensing Practice','Quarter used for patient data','Number of Registered Patients (Last Known Figure)','Number of Weighted Patients (Last Known Figure)','Average payments per registered patient','Average payments per weighted patient','Total NHS Payments to General Practice',"Deductions for Pensions, Levies and Prescription Charge Income",'Total NHS Payments to General Practice Minus Deductions']
+    includecols18 = ['NHS England and NHS Improvement (Region, local office) Code','CCG Code','Practice Code','Practice Postcode','Practice Open Date','Practice Close Date','Contract Type','Dispensing Practice','Average Number of Registered Patients','Average Number of Weighted Patients','Average payments per registered patient','Average payments per weighted patient','Total NHS Payments to General Practice',"Deductions for Pensions, Levies and Prescription Charge Income",'Total NHS Payments to General Practice Minus Deductions']
     
-    df15 = pd.read_csv('../nhs-data/nhspaymentsgp-15-16-csv.csv', encoding = "ISO-8859-1")[indcludecols15]
+    df15 = pd.read_csv('../nhs-data/nhspaymentsgp-15-16-csv.csv', encoding = "ISO-8859-1")[includecols15]
     df15 = df15[df15.NHSEnglandRegionCode != 'Q73']
 
     df16 = pd.read_csv('../nhs-data/nhspaymentsgp-16-17-csv.csv', encoding = "ISO-8859-1")
@@ -45,13 +39,13 @@ def main():
         'NumberofWeightedPatientsLastKnownFigure': 'NumberOfWeightedPatientsLastKnownFigure',
         'NumberofRegisteredPatientsLastKnownFigure': 'NumberOfRegisteredPatientsLastKnownFigure'
         })
-    df16 = df16[indcludecols15].dropna()
+    df16 = df16[includecols15].dropna()
     df16 = df16[df16.DispensingPractice != 'UNKNOWN']
     df16 = df16[df16.ContractType != 'UNKNOWN']
     df16 = df16[df16.NHSEnglandRegionCode != 'Q83']
     df16 = df16[df16.NHSEnglandRegionCode != 'Q84']
     
-    df17 = pd.read_csv('../nhs-data/nhspaymentsgp-17-18-csv.csv', encoding = "ISO-8859-1")[indcludecols17]
+    df17 = pd.read_csv('../nhs-data/nhspaymentsgp-17-18-csv.csv', encoding = "ISO-8859-1")[includecols17]
     df17 = df17.rename(columns={
         'NHS England (Region, local office) Code': 'NHSEnglandRegionCode',
         'Contract Type': 'ContractType',
@@ -60,13 +54,13 @@ def main():
         'Number of Weighted Patients (Last Known Figure)': 'NumberOfWeightedPatientsLastKnownFigure',
         'Total NHS Payments to General Practice': 'TotalNHSPaymentsToGeneralPractice_£'
     })
-    df17 = df17[indcludecols15].dropna()
+    df17 = df17[includecols15].dropna()
     df17 = df17[df17.DispensingPractice != 'UNKNOWN']
     df17 = df17[df17.ContractType != 'UNKNOWN']
     df17 = df17[df17.NHSEnglandRegionCode != 'Q83']
     df17 = df17[df17.NHSEnglandRegionCode != 'Q84']
 
-    df18 = pd.read_csv('../nhs-data/nhspaymentsgp-18-19-csv.csv', encoding = "ISO-8859-1")[indcludecols18]
+    df18 = pd.read_csv('../nhs-data/nhspaymentsgp-18-19-csv.csv', encoding = "ISO-8859-1")[includecols18]
 
     train_y = df15['TotalNHSPaymentsToGeneralPractice_£']
     train_x = df15.drop(columns=['TotalNHSPaymentsToGeneralPractice_£'])
